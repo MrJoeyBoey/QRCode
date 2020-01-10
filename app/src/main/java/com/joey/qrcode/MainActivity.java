9736353269b3749bb6fe46e4dad8b6f906524196
MrJoeyBoey <449187798@qq.com>
2020-01-10 14:43:40 +0800

    添加了代码使用示例

--- app/src/main/java/com/joey/qrcode/MainActivity.java
+++ app/src/main/java/com/joey/qrcode/MainActivity.java
@@ -1,9 +1,19 @@
 package com.joey.qrcode;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.joey.qrcodelibrary.QRCodeUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +24,35 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        QRCodeUtil.scanQRCode(MainActivity.this,0);
+                    }else {
+                        requestPermissions(new String[]{Manifest.permission.CAMERA},1);
+                    }
+                }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1){
+            if(permissions[0].equals(Manifest.permission.CAMERA) && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                QRCodeUtil.scanQRCode(MainActivity.this,0);
+            }else {
+                Toast.makeText(MainActivity.this,"拒绝该权限将无法扫描二维码",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 0 && resultCode == RESULT_OK){
+            if(data == null)return;
+            ((TextView)findViewById(R.id.text)).setText(data.getStringExtra("codedContent"));
+        }
     }
 }
