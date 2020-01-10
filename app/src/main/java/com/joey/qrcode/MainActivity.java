178b67569384f2c3cfdf4a6a46eb8e9595fa7306
MrJoeyBoey <449187798@qq.com>
2020-01-10 15:26:28 +0800

    更新了示例

--- app/src/main/java/com/joey/qrcode/MainActivity.java
+++ app/src/main/java/com/joey/qrcode/MainActivity.java
@@ -7,21 +7,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.joey.qrcodelibrary.QRCodeUtil;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnScan,btnCreate;
+    private TextView tvResultScan;
+    private ImageView ivResultCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
+
+        initView();
+        initEvent();
+    }
+
+    private void initView(){
+        btnScan = findViewById(R.id.btn_scan);
+        btnCreate = findViewById(R.id.btn_create);
+        tvResultScan = findViewById(R.id.tv_scan_result);
+        ivResultCreate = findViewById(R.id.iv_create_result);
+    }
+
+    private void initEvent(){
+        btnScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -31,6 +52,14 @@ public class MainActivity extends AppCompatActivity {
                         requestPermissions(new String[]{Manifest.permission.CAMERA},1);
                     }
                 }
+            }
+        });
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Glide.with(MainActivity.this)
+                        .load(QRCodeUtil.createQRCode("害!",1080,1080, BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)))
+                        .into(ivResultCreate);
             }
         });
     }
@@ -52,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == 0 && resultCode == RESULT_OK){
             if(data == null)return;
-            ((TextView)findViewById(R.id.text)).setText(data.getStringExtra("codedContent"));
+            tvResultScan.setText(("扫描结果:"+data.getStringExtra(QRCodeUtil.SCAN_RESULT)));
         }
     }
 }
